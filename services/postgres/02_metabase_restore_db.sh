2511366919ec9018acaf3bb6b69c76e8a2d74dc4
toddheslin <toddheslin@users.noreply.github.com>
2019-11-09 12:51:05 +1300

    Importing from binary or text format

--- services/postgres/02_metabase_restore_db.sh
+++ services/postgres/02_metabase_restore_db.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e # exit immediately if a command exits with a non-zero status.
 
-BACKUP_FILE="/docker-entrypoint-initdb.d/backup.bak"
+BACKUP_FILE="/docker-entrypoint-initdb.d/backup.sql"
+BACKUP_TYPE="sql"
 
 # check if backup file exists
 if [[ -e $BACKUP_FILE ]]; then
-    echo "Restoring backup to database"
-    pg_restore --username=postgres -c --if-exists --dbname=metabase $BACKUP_FILE > /dev/null
+    echo "Found a backup of type $BACKUP_TYPE, restoring database"
+    if [[ $BACKUP_TYPE == "sql" ]]; then
+        echo "Restoring via psql"
+        psql --username=postgres -c --if-exists --dbname=metabase -f $BACKUP_FILE > /dev/null
+    else
+        echo "Restoring via pg_restore"
+        pg_restore --username=postgres -c --if-exists --dbname=metabase $BACKUP_FILE > /dev/null
+    fi
 else
     echo "No Backup found"
 fi
